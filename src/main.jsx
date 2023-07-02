15981742a6a897e674e6563ac240964f18beee81
Juan Carlos Zambrano <jucarlos.zambrano@gmail.com>
2023-07-02 18:09:25 -0400

    se actualiza title

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login 
-      title={"Hola!! esto es el primer titulo de un proyecto enriquecedor"} 
+      title={"Hola!! esto es el primer titulo del proyecto Portal de empleos"} 
       subTitle={"Primer contenido del Portal de Empleos"} 
     />
   </React.StrictMode>,
